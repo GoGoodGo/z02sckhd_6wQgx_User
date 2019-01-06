8e314b14c3e4094f11bb24ee7f9dcc9c1c20d173
Smiling <442371047@qq.com>
2019-01-06 15:09:08 +0800

    修改详情图片比例

--- z02sckhd_6wQgx_User/Classes/Goods/Controller/View/GoodsDetialCell.swift
+++ z02sckhd_6wQgx_User/Classes/Goods/Controller/View/GoodsDetialCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 import YHTool
 
-class GoodsDetialCell: UITableViewCell {
+class GoodsDetialCell: UITableViewCell, UIWebViewDelegate {
     
     @IBOutlet weak var webView: UIWebView!
     
@@ -19,12 +19,26 @@ class GoodsDetialCell: UITableViewCell {
     
         let url = URLRequest.init(url: URL.init(string: "https://www.baidu.com")!)
         webView.loadRequest(url)
+        webView.delegate = self
+    }
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+    
+        let js = "var script = document.createElement('script');script.type = 'text/javascript';script.text = \"function ResizeImages() { var myimg,oldwidth;var maxwidth = \(WIDTH);for(i=0;i <document.images.length;i++){myimg = document.images[i];if(myimg.width > maxwidth){oldwidth = myimg.width;myimg.width = \(WIDTH - 15);}}}\";document.getElementsByTagName('head')[0].appendChild(script);"
+        
+        webView.stringByEvaluatingJavaScript(from: js)
+        webView.stringByEvaluatingJavaScript(from: "ResizeImages();")
     }
     
     // MARK: - Setter
     var detial: String? {
         didSet {
             webView.loadHTMLString((detial?.replacingOccurrences(of: "<img src=\"", with: "<img width=\(WIDTH - 15) src=\"\(BASE_URL)") ?? "暂无详情..."), baseURL: nil)
+            webView.loadHTMLString((detial?.replacingOccurrences(of: "width:", with: "width:\(WIDTH - 15)") ?? "..."), baseURL: nil)
         }
     }
     
